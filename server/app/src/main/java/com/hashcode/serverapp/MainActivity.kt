13eace6c9a893700cc3431b0104c1299c57fd488
mishotek <mtekt16@freeuni.edu.ge>
2020-08-02 20:05:14 +0400

    Using service for the server

--- server/app/src/main/java/com/hashcode/serverapp/MainActivity.kt
+++ server/app/src/main/java/com/hashcode/serverapp/MainActivity.kt
@@ -1,40 +1,31 @@
 package com.hashcode.serverapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
-import com.hashcode.serverapp.handlers.AuthHandler
-import com.hashcode.serverapp.handlers.MessageHandler
-import com.hashcode.serverapp.handlers.MessageHistoryHandler
-import com.hashcode.serverapp.handlers.PingHandler
-import com.sun.net.httpserver.HttpExchange
-import com.sun.net.httpserver.HttpHandler
-import com.sun.net.httpserver.HttpServer
 import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONObject
-import java.io.IOException
-import java.io.InputStream
-import java.net.InetSocketAddress
-import java.util.*
-import java.util.concurrent.Executors
+
 
 
 class MainActivity : AppCompatActivity() {
 
     private var serverUp = false
-    private var mHttpServer: HttpServer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val port = 5000
 
         serverButton.setOnClickListener {
-            serverUp = if(!serverUp){
-                startServer(port)
+            serverUp = if(!serverUp) {
+                startService(Intent(this, MainService::class.java))
+                serverTextView.text = getString(R.string.server_running)
+                serverButton.text = getString(R.string.stop_server)
                 true
             } else{
-                stopServer()
+                stopService(Intent(this, MainService::class.java))
+                serverTextView.text = getString(R.string.server_down)
+                serverButton.text = getString(R.string.start_server)
                 false
             }
 
@@ -46,85 +37,5 @@ class MainActivity : AppCompatActivity() {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
-    }
-
-    private fun streamToString(inputStream: InputStream): String {
-        val s = Scanner(inputStream).useDelimiter("\\A")
-        return if (s.hasNext()) s.next() else ""
-    }
-
-    private fun sendResponse(httpExchange: HttpExchange, responseText: String){
-        httpExchange.sendResponseHeaders(200, responseText.length.toLong())
-        val os = httpExchange.responseBody
-        os.write(responseText.toByteArray())
-        os.close()
-    }
-
-    private fun startServer(port: Int) {
-        try {
-            mHttpServer = HttpServer.create(InetSocketAddress(port), 0)
-            mHttpServer!!.executor = Executors.newCachedThreadPool()
-
-            mHttpServer!!.createContext("/", rootHandler)
-            mHttpServer!!.createContext("/index", rootHandler)
-
-            // Routes
-            mHttpServer!!.createContext("/ping", PingHandler().getHandler())
-            mHttpServer!!.createContext("/auth", AuthHandler(this).getHandler())
-            mHttpServer!!.createContext("/send-message", MessageHandler(this).getHandler())
-            mHttpServer!!.createContext("/messages", MessageHistoryHandler(this).getHandler())
-
-
-            mHttpServer!!.start()//startServer server;
-
-            serverTextView.text = getString(R.string.server_running)
-            serverButton.text = getString(R.string.stop_server)
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-
-    }
-
-    private fun stopServer() {
-        if (mHttpServer != null){
-            mHttpServer!!.stop(0)
-            serverTextView.text = getString(R.string.server_down)
-            serverButton.text = getString(R.string.start_server)
-        }
-    }
-
-    // Handler for root endpoint
-    private val rootHandler = HttpHandler { exchange ->
-        run {
-            // Get request method
-            when (exchange!!.requestMethod) {
-                "GET" -> {
-                    sendResponse(exchange, "Welcome to my server")
-                }
-
-            }
-        }
-
-    }
-
-    private val messageHandler = HttpHandler { httpExchange ->
-        run {
-            when (httpExchange!!.requestMethod) {
-                "GET" -> {
-                    // Get all messages
-                    sendResponse(httpExchange, "Would be all messages stringified json")
-                }
-                "POST" -> {
-                    val inputStream = httpExchange.requestBody
-
-                    val requestBody = streamToString(inputStream)
-                    val jsonBody = JSONObject(requestBody)
-                    // save message to database
-
-                    //for testing
-                    sendResponse(httpExchange, jsonBody.toString())
-                }
-            }
-        }
     }
 }
---
+++ server/app/src/main/java/com/hashcode/serverapp/MainService.kt
@@ -0,0 +1,114 @@
+package com.hashcode.serverapp
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import com.hashcode.serverapp.handlers.AuthHandler
+import com.hashcode.serverapp.handlers.MessageHandler
+import com.hashcode.serverapp.handlers.MessageHistoryHandler
+import com.hashcode.serverapp.handlers.PingHandler
+import com.sun.net.httpserver.HttpExchange
+import com.sun.net.httpserver.HttpHandler
+import com.sun.net.httpserver.HttpServer
+import org.json.JSONObject
+import java.io.IOException
+import java.io.InputStream
+import java.net.InetSocketAddress
+import java.util.*
+import java.util.concurrent.Executors
+
+class MainService() : Service() {
+
+    private val port = 5000
+    private var mHttpServer: HttpServer? = null
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        startServer(port)
+        return START_STICKY;
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        stopServer()
+    }
+
+    private fun streamToString(inputStream: InputStream): String {
+        val s = Scanner(inputStream).useDelimiter("\\A")
+        return if (s.hasNext()) s.next() else ""
+    }
+
+    private fun sendResponse(httpExchange: HttpExchange, responseText: String){
+        httpExchange.sendResponseHeaders(200, responseText.length.toLong())
+        val os = httpExchange.responseBody
+        os.write(responseText.toByteArray())
+        os.close()
+    }
+
+    private fun startServer(port: Int) {
+        try {
+            mHttpServer = HttpServer.create(InetSocketAddress(port), 0)
+            mHttpServer!!.executor = Executors.newCachedThreadPool()
+
+            mHttpServer!!.createContext("/", rootHandler)
+            mHttpServer!!.createContext("/index", rootHandler)
+
+            // Routes
+            mHttpServer!!.createContext("/ping", PingHandler().getHandler())
+            mHttpServer!!.createContext("/auth", AuthHandler(this).getHandler())
+            mHttpServer!!.createContext("/send-message", MessageHandler(this).getHandler())
+            mHttpServer!!.createContext("/messages", MessageHistoryHandler(this).getHandler())
+
+
+            mHttpServer!!.start()//startServer server;
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    private fun stopServer() {
+        if (mHttpServer != null){
+            mHttpServer!!.stop(0)
+        }
+    }
+
+    // Handler for root endpoint
+    private val rootHandler = HttpHandler { exchange ->
+        run {
+            // Get request method
+            when (exchange!!.requestMethod) {
+                "GET" -> {
+                    sendResponse(exchange, "Welcome to my server")
+                }
+
+            }
+        }
+
+    }
+
+    private val messageHandler = HttpHandler { httpExchange ->
+        run {
+            when (httpExchange!!.requestMethod) {
+                "GET" -> {
+                    // Get all messages
+                    sendResponse(httpExchange, "Would be all messages stringified json")
+                }
+                "POST" -> {
+                    val inputStream = httpExchange.requestBody
+
+                    val requestBody = streamToString(inputStream)
+                    val jsonBody = JSONObject(requestBody)
+                    // save message to database
+
+                    //for testing
+                    sendResponse(httpExchange, jsonBody.toString())
+                }
+            }
+        }
+    }
+}
